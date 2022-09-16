3a937f42b5c8ba30ddca2cb28b87bfbf84c39bc3
Kiswelrg <916650903@qq.com>
2022-09-16 20:07:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pid=`ps -ef|grep "sh /home/kiswelrg/Projects/scripts/everysecond.sh"| grep -v "grep"|awk '{print $2}'`
+pid=`ps -ef|grep "sh /YourFilePath/everysecond.sh"| grep -v "grep"|awk '{print $2}'`
 
 if [ "$pid" != ""]
 then
@@ -10,9 +10,9 @@ fi
 
 echo "starting ..."
 
-nohup sh /home/kiswelrg/Projects/scripts/everysecond.sh > /home/kiswelrg/Projects/scripts/lianwang.out 2>&1 &
+nohup sh /YourFilePath/everysecond.sh > /YourFilePath/lianwang.out 2>&1 &
 
-pid=`ps -ef|grep "sh /home/kiswelrg/Projects/scripts/everysecond.sh"| grep -v "grep"|awk '{print $2}'`
+pid=`ps -ef|grep "sh /YourFilePath/everysecond.sh"| grep -v "grep"|awk '{print $2}'`
 
-echo ${pid} > /home/kiswelrg/Projects/scripts/pid1.out
+echo ${pid} > /YourFilePath/pid1.out
 echo "everysecond started at pid: "${pid}
